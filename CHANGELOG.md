ebf6c2bc0f10cbdfd28cbe09884ca42bf6fc5c02
tomsq <finixit@email.cz>
2026-01-15 18:47:54 +0100

    feat: add releasing

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# lettr
+
+## 0.0.0-alpha-20260115173114
+
+### Patch Changes
+
+- Initial release
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: 'src/index.ts',
-})
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  clean: true,
+});
